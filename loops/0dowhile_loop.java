4855c508b9241d4eb655f14b1f433dd93a330c08
Durgeshkumarddddd <durgeshkumarddddd123@gmail.com>
2024-11-01 23:59:24 +0530

    loops

---
+++ loops/0dowhile_loop.java
@@ -0,0 +1,9 @@
+package loops;
+class dowhileloop{
+    public static void main(String[] args){
+        int num = 1;
+        do{
+            System.out.println("num: " + num);
+            num++;
+        }while(num==0);
+}}
