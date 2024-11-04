b1bb7c0611cba2aa6a3b1064e91bfa047971dfe1
Durgeshkumarddddd <durgeshkumarddddd123@gmail.com>
2024-11-04 23:18:17 +0530

    m

--- loops/2_palindrome_no.java
+++ loops/2_palindrome_no.java
@@ -9,12 +9,14 @@ class palindrome_no {
            
     System.out.println("Enter a number");
     int num = takeinput();
+    int n = num ;
     int reverse = 0 ;
-    while(num > 0){
-        int lastdigit = num % 10;
-          num = num / 10;
+    while(n > 0){
+        int lastdigit = n % 10;
+          n = n / 10;
         reverse = reverse*10 + lastdigit;
     }
+    System.out.println("num"+ num + "reverse " + reverse);
     if(num == reverse){
         System.out.println("NUmber is palindrome");
     }else{ 
