cfb12b8d3ea065e8a3f56b58e7375eef0870d8fa
Durgeshkumarddddd <durgeshkumarddddd123@gmail.com>
2025-04-15 00:47:29 +0530

    doublyLL

---
+++ LinkedList/13_DoublyLL.java
@@ -0,0 +1,94 @@
+class DoublyLL {
+   class Node {
+        int data ;
+        Node next ;
+        Node prev ;
+        Node (int data){
+            this.data = data ;
+            this.next = null;
+            this.prev = null ;
+        }
+    }
+    public static Node head;
+    public static Node tail ;
+    public static int Size ;
+
+    // Insert First in doubly  link List
+    public void insertFirst(int data){
+        Node newNode = new Node(data);
+        Size++ ;
+
+        if(head == null){
+            head = tail = newNode ;
+            return ;
+
+        }
+        newNode.next = head ;
+        head.prev = newNode ;
+        head = newNode ;
+    }
+    
+    // Remove Last Node from ll
+    public Node RemoveLast(Node head){
+        if(head == null){
+            return null ;
+        }
+        Node prevNode = null ;
+        Node curr = head ;
+        while(curr.next != null){
+            prevNode = curr ;
+            curr = curr.next ;
+        }
+        curr.prev = null ;
+        prevNode.next = curr.next ;
+        Size--; 
+        
+        return head ;
+
+    }
+
+    // Remove First 
+    public int RemoveFirst( ){
+        if(head == null){
+            System.out.println("Empty Doubly linked list");
+            return Integer.MIN_VALUE ;
+        }
+        // when one Node is present then 
+        if(Size == 1){
+            int val = head.data;
+            Size-- ;
+            head = tail =  null ;
+            return val ;
+
+        }
+
+        int val = head.data ;
+        head = head.next ;
+        head.prev = null ;
+        Size-- ;
+        return val ;
+    }
+    // Print Doubly ll
+    public void printList( ){
+        Node temp = head ;
+
+        while(temp != null){
+            System.out.print(temp.data+"<->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+    
+    public static void main(String[] args){
+        DoublyLL dll = new DoublyLL();
+        dll.insertFirst(1);
+        // dll.insertFirst(2);
+        // dll.insertFirst(3);
+        dll.printList(); 
+        // dll.RemoveLast(head);
+        // dll.printList();
+        dll.RemoveFirst();
+        dll.printList();
+
+    }
+}
