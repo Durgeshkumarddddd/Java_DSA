9242cb9af263485367ca1987ba358d52773ec236
Durgeshkumarddddd <durgeshkumarddddd123@gmail.com>
2025-08-06 00:00:26 +0530

    n

--- infosys-Problems/LinkedList/1_InsertAnyPosition.java
+++ infosys-Problems/LinkedList/1_InsertAnyPosition.java
@@ -9,13 +9,59 @@ class InsertAnyPosition {
             this.next = null ;
         }
     }
+    public static Node head ;
+    public static Node tail ;
+
+    // Insert first 
+    public void insertFirst(int data) {
+        Node newNode = new Node(data);
+        if (head == null){
+            head = tail = newNode ;
+            return ;
+        }
+        newNode.next = head ;
+        head = newNode ;
+    }
+
+    // Insert Any position of LL 
+    public void InsertAtPosition(int data , int position){
+        Node newNode = new Node(data);
+        if ( head == null){
+            head = tail = newNode ;
+            return ;
+        }
+        Node prev = null ;
+        Node curr = head ;
+        for ( int i = 0 ; i < position ; i++){
+            prev = curr ;
+            curr = curr.next ;
+        }
+        prev.next = newNode ;
+        newNode.next = curr  ;
+        
+    }
+
+    // Traverse Linked List 
+
+    public void printLL(){
+        Node temp = head ;
+        while ( temp != null ){
+            System.out.print(temp.data + "->");
+            temp = temp.next ;
+        }
+        System.out.println();
+    }
+
     public static void main(String... args){
         InsertAnyPosition ll = new InsertAnyPosition();
-        Node newNode = new Node(5);
-        newNode.next = new Node(6);
-        newNode.next.next = new Node(7);
+        ll.insertFirst(1);
+        ll.insertFirst(2);
+        ll.insertFirst(3);
+        ll.printLL();
 
-        
+        ll.InsertAtPosition(5, 2);
+        ll.printLL();
+
 
     }
 }
